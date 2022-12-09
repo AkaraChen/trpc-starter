484ccfb0275a95760176af97bcc138333dac7d4d
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2022-12-09 10:47:02 +0800

    ✨ feat: add tailwind for web

---
+++ packages/web/postcss.config.js
@@ -0,0 +1,7 @@
+/* eslint-disable unicorn/prefer-module */
+module.exports = {
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {}
+    }
+};
---
+++ packages/web/tailwind.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable unicorn/prefer-module */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}'
+    ],
+    theme: {
+        extend: {}
+    },
+    plugins: []
+};
